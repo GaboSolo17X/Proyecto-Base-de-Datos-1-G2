899b1c96ed174a70be7cc9e9348d2785c8ef8f2e
id-Yashua <48071205+id-Yashua@users.noreply.github.com>
2022-07-23 22:35:24 -0600

    Update Proyecto1.sql

--- Proyecto1.sql
+++ Proyecto1.sql
@@ -15,7 +15,6 @@ INSERT INTO SUCURSAL VALUES ('SUC01', 'Sport Life Ventu', 'Bloque A', 'Las Casit
 INSERT INTO SUCURSAL VALUES ('SUC02', 'Sport Life Marriot','-','-', 'Bvd.Juan PabloII');
 
 SELECT*FROM SUCURSAL;
---ESTE ES UN COMENTARIO DE PRUEBA
 
 --2)Creando la tabla EMPLEADO
 CREATE TABLE EMPLEADO
@@ -163,6 +162,28 @@ CONSTRAINT PK_PRODUCTO PRIMARY KEY (CodigoPro),
 CONSTRAINT CHK_Precio CHECK (Precio>0)
 )
 
+-----------------------------------------INSERT PRODUCTO----------------------------------------------------
+INSERT INTO PRODUCTO VALUES ('P1', 'Calmante muscular', 'crema', 'Sabon', '150');
+INSERT INTO PRODUCTO VALUES ('P2', 'Aceite de ducha', 'aceite', 'Sabon', '200');
+INSERT INTO PRODUCTO VALUES ('P3', 'Jabon de cuerpo', 'jabon', 'Sabon', '300');
+INSERT INTO PRODUCTO VALUES ('P4', 'Exfoliante corporal', 'crema', 'Sabon', '300');
+INSERT INTO PRODUCTO VALUES ('P5', 'Gel de ducha sport', 'jabon', 'Etnia', '250');
+INSERT INTO PRODUCTO VALUES ('P6', 'Fragancia corporal', 'perfume', 'Etnia', '350');
+INSERT INTO PRODUCTO VALUES ('P7', 'Crema hidratante', 'crema', 'Neutrogena', '400');
+INSERT INTO PRODUCTO VALUES ('P8', 'Crema para afeitar', 'crema', 'Neutrogena', '200');
+INSERT INTO PRODUCTO VALUES ('P9', 'Exfoliante corporal', 'crema', 'Neutrogena', '350');
+INSERT INTO PRODUCTO VALUES ('P10', 'Crema de ducha', 'crema', 'Body Natur', '280');
+INSERT INTO PRODUCTO VALUES ('P11', 'Locion facial', 'perfume', 'ASH', '300');
+INSERT INTO PRODUCTO VALUES ('P12', 'Exfoliante facial', 'crema', 'ASH', '350');
+INSERT INTO PRODUCTO VALUES ('P13', 'Exfoliante corporal', 'crema', 'ASH', '450');
+INSERT INTO PRODUCTO VALUES ('P14', 'Proteina', 'huevo', 'Ovofull', '500');
+INSERT INTO PRODUCTO VALUES ('P15', 'Proteina instantanea', 'carne', 'Ovofull', '600');
+INSERT INTO PRODUCTO VALUES ('P16', 'Proteina instantanea Plus', 'Carne', 'Ovofull', '800');
+INSERT INTO PRODUCTO VALUES ('P17', 'Proteina', 'huevo', 'Weider', '550');
+INSERT INTO PRODUCTO VALUES ('P18', 'Proteina premium', 'carne', 'Weider', '850');
+INSERT INTO PRODUCTO VALUES ('P19', 'Proteina Micelle', 'carne', 'Casein', '550');
+INSERT INTO PRODUCTO VALUES ('P20', 'Proteina en batido', 'frutas', 'Vikika', '700');
+-----------------------------------------FIN INSERT PRODUCTO----------------------------------------------------
 
 --14)Creando la tabla PROVEEDOR
 CREATE TABLE PROVEEDOR
@@ -177,7 +198,17 @@ Bloque            VARCHAR(50) NOT NULL,
 CONSTRAINT PK_PROVEEDOR PRIMARY KEY (Id_Proveedor)
 )
 
-
+-----------------------------------------INSERT PROVEEDOR----------------------------------------------------
+INSERT INTO PROVEEDOR VALUES ('01', 'Sabon', '22803808', 'Los Laureles', '23', '1');
+INSERT INTO PROVEEDOR VALUES ('02', 'Etnia', '24556565', 'Los Pinos', '21', '2');
+INSERT INTO PROVEEDOR VALUES ('03', 'Neutrogena', '22556789', 'Toncontin', '12', '3');
+INSERT INTO PROVEEDOR VALUES ('04', 'Body Natur', '22123456', 'Miraflores', '25', '4');
+INSERT INTO PROVEEDOR VALUES ('05', 'ASH', '22987654', 'Loma Linda', '4', '2');
+INSERT INTO PROVEEDOR VALUES ('06', 'Ovofull', '22304455', 'Bella Vista', '2', '4');
+INSERT INTO PROVEEDOR VALUES ('07', 'Weider', '22404031', 'Bonita Flor', '9', '6');
+INSERT INTO PROVEEDOR VALUES ('08', 'Casein', '22694203', 'Belleza Divina', '15', '3');
+INSERT INTO PROVEEDOR VALUES ('09', 'Vikika', '22110007', 'San Juan', '13', '1');
+-----------------------------------------FIN INSERT PROVEEDOR----------------------------------------------------
 
 --15)Creando la tabla CLIENTE
 CREATE TABLE CLIENTE
@@ -199,8 +230,6 @@ CONSTRAINT CHK_RazonCliente CHECK (Razon in ('MED','PER','OTRO')),
 CONSTRAINT FK_ClienteEmp FOREIGN KEY (DniEmpleado) REFERENCES EMPLEADO(DNI),
 CONSTRAINT FK_ClienteSucu FOREIGN KEY (IdSucursal) REFERENCES SUCURSAL(Id_Sucursal)
 )
-
-
 
 --16)Creando la tabla FACTURA
 CREATE TABLE FACTURA
@@ -247,7 +276,16 @@ NombreSer         VARCHAR(50) NOT NULL CONSTRAINT NombreSer UNIQUE,
 CONSTRAINT PK_SERVICIO PRIMARY KEY (Id_Servicio)
 )
 
-
+-----------------------------------------INSERT SERVICIO----------------------------------------------------
+INSERT INTO SERVICIO VALUES ('1', 'Cardio');
+INSERT INTO SERVICIO VALUES ('2', 'Pesas');
+INSERT INTO SERVICIO VALUES ('3', 'Spinning');
+INSERT INTO SERVICIO VALUES ('4', 'Zumba');
+INSERT INTO SERVICIO VALUES ('5', 'Aerobicos');
+INSERT INTO SERVICIO VALUES ('6', 'Yoga');
+INSERT INTO SERVICIO VALUES ('7', 'Spa');
+INSERT INTO SERVICIO VALUES ('8', 'Masajes');
+-----------------------------------------FIN INSERT SERVICIO----------------------------------------------------
 
 --19)Creando la tabla SERVICIO EJERCICIO
 CREATE TABLE SERV_EJERCICIO
@@ -259,7 +297,14 @@ CONSTRAINT PK_SERVEJE PRIMARY KEY (Id_Servicio),
 CONSTRAINT FK_EjeServicio FOREIGN KEY (Id_Servicio) REFERENCES SERVICIO(Id_Servicio)
 )
 
-
+-----------------------------------------INSERT SERV_EJERCICIO----------------------------------------------------
+INSERT INTO SERV_EJERCICIO VALUES ('1', 'Cardio');
+INSERT INTO SERV_EJERCICIO VALUES ('2', 'Pesas');
+INSERT INTO SERV_EJERCICIO VALUES ('3', 'Spinning');
+INSERT INTO SERV_EJERCICIO VALUES ('4', 'Zumba');
+INSERT INTO SERV_EJERCICIO VALUES ('5', 'Aerobicos');
+INSERT INTO SERV_EJERCICIO VALUES ('6', 'Yoga');
+-----------------------------------------FIN INSERT SERV_EJERCICIO----------------------------------------------------
 
 --20)Creando la tabla SERVICIO VIP
 CREATE TABLE SERV_VIP
@@ -271,7 +316,10 @@ CONSTRAINT PK_VIPEJE PRIMARY KEY (Id_Servicio),
 CONSTRAINT FK_VIPServicio FOREIGN KEY (Id_Servicio) REFERENCES SERVICIO(Id_Servicio)
 )
 
-
+-----------------------------------------INSERT SERV_VIP----------------------------------------------------
+INSERT INTO SERV_VIP VALUES ('7', 'Matutino');
+INSERT INTO SERV_VIP VALUES ('8', 'Vespertino');
+-----------------------------------------FIN INSERT SERV_VIP----------------------------------------------------
 
 --21)Creando la tabla AREA
 CREATE TABLE AREA
@@ -284,7 +332,16 @@ CONSTRAINT PK_AREA PRIMARY KEY (CodEspacio),
 CONSTRAINT FK_AREAServi FOREIGN KEY (IdServicio) REFERENCES SERVICIO(Id_Servicio)
 )
 
-
+-----------------------------------------INSERT AREA----------------------------------------------------
+INSERT INTO AREA VALUES ('A1', 'Zona de cardio', '1');
+INSERT INTO AREA VALUES ('A2', 'Zona de Pesas', '2');
+INSERT INTO AREA VALUES ('A3', 'Zona de spinning', '3');
+INSERT INTO AREA VALUES ('A4', 'Zona de bailes', '4');
+INSERT INTO AREA VALUES ('A5', 'Zona de aerobicos', '5');
+INSERT INTO AREA VALUES ('A6', 'Zona de yoga', '6');
+INSERT INTO AREA VALUES ('A7', 'Zona de spa', '7');
+INSERT INTO AREA VALUES ('A8', 'Zona de masajes', '8');
+-----------------------------------------FIN INSERT AREA----------------------------------------------------
 
 --22)Creando la tabla CLASE
 CREATE TABLE CLASE
@@ -453,3 +510,25 @@ CONSTRAINT FK_SumiProv FOREIGN KEY (Id_Proveedor) REFERENCES PROVEEDOR(Id_Provee
 CONSTRAINT FK_SumiProd FOREIGN KEY (CodigoPro) REFERENCES PRODUCTO(CodigoPro)
 )
 
+-----------------------------------------INSERT SUMINISTRA----------------------------------------------------
+INSERT INTO SUMINISTRA VALUES ('01', 'P1');
+INSERT INTO SUMINISTRA VALUES ('01', 'P2');
+INSERT INTO SUMINISTRA VALUES ('01', 'P3');
+INSERT INTO SUMINISTRA VALUES ('01', 'P4');
+INSERT INTO SUMINISTRA VALUES ('02', 'P5');
+INSERT INTO SUMINISTRA VALUES ('02', 'P6');
+INSERT INTO SUMINISTRA VALUES ('03', 'P7');
+INSERT INTO SUMINISTRA VALUES ('03', 'P8');
+INSERT INTO SUMINISTRA VALUES ('03', 'P9');
+INSERT INTO SUMINISTRA VALUES ('04', 'P10');
+INSERT INTO SUMINISTRA VALUES ('05', 'P11');
+INSERT INTO SUMINISTRA VALUES ('05', 'P12');
+INSERT INTO SUMINISTRA VALUES ('05', 'P13');
+INSERT INTO SUMINISTRA VALUES ('06', 'P14');
+INSERT INTO SUMINISTRA VALUES ('06', 'P15');
+INSERT INTO SUMINISTRA VALUES ('06', 'P16');
+INSERT INTO SUMINISTRA VALUES ('07', 'P17');
+INSERT INTO SUMINISTRA VALUES ('07', 'P18');
+INSERT INTO SUMINISTRA VALUES ('08', 'P19');
+INSERT INTO SUMINISTRA VALUES ('09', 'P20');
+-----------------------------------------FIN INSERT SUMMINISTRA----------------------------------------------------
